04c5ac60222014f09f7aa5517b983621766a3b62
jian you qi <scienceqjy@gmail.com>
2026-04-23 23:55:49 +0800

    docs: Update README.md to full English version

---
+++ update_readme.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d D:\Software\Huawei\DevEcoStudioProjects\ColorCycling_HOS_Git2.2
+git add README.md
+git commit -m "docs: Update README.md to full English version"
+git push origin main
+echo Done!
+pause
